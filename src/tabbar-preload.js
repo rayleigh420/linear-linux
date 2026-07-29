14b9e7959f7621dad2a45a4c1ad8f626db64ecf8
dyuifly <duy.le@popfly.com>
2026-07-29 13:30:45 +0700

    refactor: refactoring to electron-vite

--- src/tabbar-preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('tabAPI', {
-    onTabsUpdated: (cb) => ipcRenderer.on('tabs-updated', (_, tabs) => cb(tabs)),
-    createTab: () => ipcRenderer.send('tab-create'),
-    closeTab: (index) => ipcRenderer.send('tab-close', index),
-    switchTab: (index) => ipcRenderer.send('tab-switch', index),
-});
